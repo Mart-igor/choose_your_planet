b71ce41538f0fa60e50f7f4177611350ff8b018b
Igor <martynov.igor2020ty@gmail.com>
2024-11-16 03:03:42 +0300

    models urls views index base

---
+++ main/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = models.Index(fields=['slug'])
+
+    def __str__(self):
+        return self.name
+    
+    # def get_absolute_url(self):
+    #     return reverse("model_detail", kwargs={"category_slug": self.slug})
+    
+class Celestial(models.Model):
+    category = models.ForeignKey(Categories, on_delete=models.PROTECT, related_name='celestial')
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    discount = models.DecimalField(max_digits=4, default=0.00, decimal_places=2)
+    created =  models.TimeField(auto_now_add=True)
+    updated = models.TimeField(auto_now=False)
+
+    class Meta:
+        ordering = ['name']
+        indexes = models.Index(fields=['slug'])
+
+    def __str__(self):
+        return self.name
+    
---
+++ main/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Celestial, Categories
+
+# Create your views here.
+def index(request):
+    celestial = Celestial.objects.filter(available=True)
+    return render(request, 'main/index.html', {'objects': celestial})
+
+def celestial_list(request, category_slug=None):
+    category = None
+    categories = Categories.objects.all()
+    celestial = Celestial.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Categories, slug=category_slug)
+        # celestial = Celestial.objects.filter(category=category)
+    return render(request, 'main/celestial.html', {'category':category,
+                                                   'categories': categories,
+                                                   'celestial': celestial})
+
+def detail_slug(request, celestial_slug):
+    celesta = get_object_or_404(Celestial, slug=celestial_slug)
+    return render(request, 'main/detail.html', {'celesta': celesta})
