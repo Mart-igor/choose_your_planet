700b9e6d9c69b1f95b807b13c793380249841cbf
Igor <martynov.igor2020ty@gmail.com>
2024-11-19 23:30:29 +0300

    a lot new

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Celestial, Categories
 
 # Register your models here.
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name', )}
+
+
+@admin.register(Celestial)
+class CelestialAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price',
+                    'available', 'created', 'updated',
+                    'discount']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available', 'discount']
+    prepopulated_fields = {'slug': ('name',)}
+
+    
